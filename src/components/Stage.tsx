3393a5045de002138e4bee132fc7fc5113a3ab75
namawasa <namawasa2@gmail.com>
2025-07-12 14:11:57 +0900

    ロゴ変更&チェック機能追加

--- src/components/Stage.tsx
+++ src/components/Stage.tsx
@@ -1,7 +1,10 @@
 
+import { useAtom } from "jotai"
 import type { StageInfo } from "../types/StageInfo"
 import { getMinuteDifference } from "../utils/dateUtil"
 import styles from "./Stage.module.css"
+import { checkedAtom } from "../libs/jotai"
+import { multiStyles } from "../utils/stringUtil"
 
 type StageProps = {
   stageInfo: StageInfo
@@ -13,13 +16,31 @@ function Stage({ stageInfo }: StageProps) {
   const endDate = new Date('2025-08-01 ' + stageInfo.end_time)
   const startMinute = getMinuteDifference(eventStartDate, startDate)
   const time = getMinuteDifference(startDate, endDate)
+
+  const [checkedList, setCheckedList] = useAtom(checkedAtom)
+
+  const onClickStage = () => {
+    if (checkedList.includes(stageInfo.id_timetable)) {
+      setCheckedList(val => val.filter(item => item !== stageInfo.id_timetable))
+    }
+    else {
+      setCheckedList(val => {
+        const nval = [...val]
+        nval.push(stageInfo.id_timetable)
+        return nval
+      })
+    }
+  }
+
   return (
     <div
-      className={styles.stage}
+      className={multiStyles(styles.stage, checkedList.includes(stageInfo.id_timetable) ? styles.checked : '')}
       style={{
         width: `${(time * 5) - 2}px`,
         left: `${startMinute * 5}px`
-      }}>
+      }}
+      onClick={onClickStage}
+    >
       {stageInfo.title}
     </div>
   )
--- src/libs/jotai.ts
+++ src/libs/jotai.ts
@@ -1,4 +1,6 @@
 import { atom } from "jotai";
 import type { DayCategory } from "../types/DayCategory";
+import { atomWithStorage } from "jotai/utils";
 
 export const selectedDayAtom = atom<DayCategory>('2025-08-01')
+export const checkedAtom = atomWithStorage<number[]>('checked', [])
